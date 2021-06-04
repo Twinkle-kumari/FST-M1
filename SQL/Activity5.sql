32fdbf25d6418da71c599521b8379bb2a665d5ab
Twinkle-kumari <52483602+Twinkle-kumari@users.noreply.github.com>
2021-06-04 19:06:14 +0530

    Add files via upload

---
+++ SQL/Activity5.sql
@@ -0,0 +1,10 @@
+Activity5
+
+Update salesman
+set grade=200 where salesman_city='Hyderabad';
+
+Update salesman
+set grade=300 where salesman_name='Twinkle';
+
+Update salesman
+set salesman_name='Peter' where salesman_id=5;
